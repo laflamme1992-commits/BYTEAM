a71bee7f72d696acfb675303f7c17ba1292e5f72
Михаил Тепляшин <mihailteplasin@MacBookAir.netis.cc>
2026-08-31 00:09:16 +0300

    Увеличен логотип

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -33,7 +33,7 @@ export function Sidebar() {
     <aside className="w-56 bg-[#0f1622]/90 backdrop-blur-md border-r border-white/5 h-screen fixed left-0 top-0 p-4 flex flex-col">
       {/* Логотип */}
       <div className="flex flex-col items-center mb-8 px-2">
-  <img src="/logo.png" alt="BYTEAM" className="h-35 w-auto" />
+  <img src="/logo.png" alt="BYTEAM" className="h-40 w-auto" />
   <span className="text-[10px] text-blue-400 bg-blue-400/10 px-2 py-0.5 rounded-full border border-blue-400/20 mt-1">
     БЕТА
   </span>
